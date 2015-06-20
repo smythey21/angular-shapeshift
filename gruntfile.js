47fb93fc43b30df61e628f99635583b4819ad705
smythey21 <smythey21@yahoo.com>
2015-06-19 20:33:23 -0400

    Adding grunt

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! angular-shapeshift <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/angular-shapeshift.min.js': ['app/directives/shapeshift.js']
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'dist/angular-shapeshift.min.css': 'app/css/shapeshift.css'
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
